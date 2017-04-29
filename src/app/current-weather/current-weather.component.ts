ce586504c2ed5ab62c71a64afdfb8b0590152f60
Prashant Palikhe <prashpal09@gmail.com>
2017-04-29 08:51:47 +0200

    WIP: Current weather component

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Current} from "../weather-card/current.model";
 
 @Component({
-  selector: 'app-current-weather',
+  selector: 'current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  @Input() weather: Current;
 
   constructor() { }
 
